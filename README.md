c75f91c71c1d92d25152a617441db571387d769e
Ivan Montiel <idmontie@asu.edu>
2015-11-16 22:41:43 -0700

    Deploy application using mup and rename to plipable
    Also add explanitory gif
    
    Close #30, close #17, close #8

--- README.md
+++ README.md
@@ -17,22 +17,30 @@ This is a Meteor application. You can run this application using:
 
 ```sh
 cd src
-meteor --setting settings.json
+meteor --setting settings.local.json
 ```
 
 
 ## Settings
 
-A `settings.example.json` file is in `/src/` as an example of what your `settings.json` file should look like. You will need to add your own Twitter API tokens in order to get Twitter logins to work. You will also need a Youtube API token.
+A `settings.example.json` file is in `/src/` as an example of what your `settings.local.json` file should look like. You will need to add your own Twitter API tokens in order to get Twitter logins to work. You will also need a Youtube API token.
 
 
 # Deployment
 
-A `settings.prod.json` file is recommended to have when deploying to production, in order to keep your development and production settings separate.
+A `settings.json` file is recommended to have when deploying to production, in order to keep your development and production settings separate.
 
 Deploy using:
 
 ```sh
 cd src
-meteor deploy --settings settings.prod.json plipit.meteor.com
+meteor deploy --settings settings.json plipit.meteor.com
+```
+
+OR
+
+If you have the mup.json configured:
+
+```sh
+mup deploy
 ```
--- src/client/views/pages/splash-page.jsx
+++ src/client/views/pages/splash-page.jsx
@@ -1,19 +1,35 @@
 SplashPage = React.createClass({
+  componentDidMount() {
+    var targetUrl = '/previews/high-res-cat-super-trimmed.gif';
+    var img = new Image();
+    img.onload = function() {
+      var $img = $(img).addClass('splash-page__preview-image').addClass('img-responsive');
+      $('#splash-cat').replaceWith($img);
+    };
+    img.src = targetUrl;
+  },
   render() {
     return (
       <div className="splash-page">
-        <img className="splash-page__logo" src="/logos/logo-reverse.png" />
-        <h1 className="splash-page__heading">{TAPi18n.__('app_name')}</h1>
-        <h2 className="splash-page__description">{TAPi18n.__('app_description')}</h2>
-
         <div className="row">
-          <div className="col s6 offset-s3">
-            <LoginComponent />
+          <div className="col m4 offset-m1 hide-on-small-only">
+            <img id="splash-cat" src="/previews/high-res-cat-super-trimmed.png" className="splash-page__preview-image img-responsive" />
+          </div>
+          <div className="col m6 s12">
+            <img className="splash-page__logo" src="/logos/logo-reverse.png" />
+            <h1 className="splash-page__heading">{TAPi18n.__('app_name')}</h1>
+            <h2 className="splash-page__description">{TAPi18n.__('app_description')}</h2>
 
-            <p>Or, <a href="/s">search for videos</a> without signing in</p>
+            <div className="row">
+              <div className="col s6 offset-s3">
+                <LoginComponent />
+
+                <p>Or, <a href="/s">search for videos</a> without signing in</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
   }
-})
+});
