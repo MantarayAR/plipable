1404f38296e1adf8c0bd3e1bee4977a0a05eacb6
Ivan Montiel <idmontie@asu.edu>
2015-11-19 20:02:26 -0700

    Fix mobile config

--- src/mobile-config.js
+++ src/mobile-config.js
@@ -1,24 +1,40 @@
-// App.info({
-//   id: 'com.plipable.app',
-//   name: 'Plipable',
-//   description: 'Timestamped video reactions',
-//   author: 'Mantaray AR LLC',
-//   email: 'ivanmontiel@mantarayar.com',
-//   website: 'http://www.plipable.com'
-// });
+App.info({
+  id: 'com.plipable.app',
+  name: 'Plipable',
+  version: '0.0.1',
+  description: 'Timestamped video reactions',
+  author: 'Mantaray AR LLC',
+  email: 'ivanmontiel@mantarayar.com',
+  website: 'http://www.plipable.com'
+});
 
 App.setPreference('BackgroundColor', '0xFF555BFF');
 App.setPreference('HideKeyboardFormAccessoryBar', true);
 App.setPreference('Orientation', 'default');
 App.setPreference('Orientation', 'all', 'ios');
 
+// Self
+App.accessRule('*.plipable.com');
 
+// Google
+App.accessRule('*.google.com');
+App.accessRule('fonts.gstatic.com');
+App.accessRule('*.google-analytics.com');
+
+// Youtube
+App.accessRule('*.youtube.com');
 App.accessRule('*.ytimg.com');
 App.accessRule('*.googlevideo.com');
+App.accessRule('*.doubleclick.net');
 App.accessRule('*.g.doubleclick.net');
 App.accessRule('*.googlesyndication.com');
-App.accessRule('*.twitter.com');
-App.accessRule('*.giphy.com');
-App.accessRule('*.youtube.com');
 
+// Twitter
+App.accessRule('*.twitter.com');
+App.accessRule('pbs.twimg.com');
+
+// Giphy
+App.accessRule('*.giphy.com');
+
+// Karida
 App.accessRule('*.kadira.io');
