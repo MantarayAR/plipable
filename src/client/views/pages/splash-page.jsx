504defebb6c6154dc38f32358328b071933ea809
Ivan Montiel <idmontie@asu.edu>
2015-11-21 17:16:37 -0700

    Fix iphone on home page, close #33

--- src/client/views/pages/splash-page.jsx
+++ src/client/views/pages/splash-page.jsx
@@ -1,24 +1,41 @@
 SplashPage = React.createClass({
   componentDidMount() {
-    var targetUrl = '/previews/high-res-cat-super-trimmed.gif';
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+
+    var targetUrl = '/previews/simon.gif';
     var img = new Image();
     img.onload = function() {
-      var $img = $(img).addClass('splash-page__preview-image').addClass('img-responsive');
+      var $img = $(img).attr('id', 'splash-cat').addClass('splash-page__preview-image').addClass('img-responsive');
       $('#splash-cat').replaceWith($img);
-    };
+      this.handleResize();
+    }.bind(this);
     img.src = targetUrl;
+  },
+  handleResize: function(e) {
+    var width = $('#iphone').width();
+    var innerWidth = width * (253.0/305.0);
+    var innerMarginTop = width * (80.0/305.0);
+    $('#splash-cat').css({width: innerWidth + 'px', 'marginTop': innerMarginTop + 'px' });
+  },
+  componentWillUnmount: function() {
+    window.removeEventListener('resize', this.handleResize);
   },
   render() {
     return (
       <div className="splash-page">
         <div className="row">
           <div className="col m4 offset-m1 hide-on-small-only">
+            &nbsp;
             <a className="splash-page__preview-image-link" href="/v/SSKatciX3hA">
-              <img id="splash-cat" src="/previews/high-res-cat-super-trimmed.png" className="splash-page__preview-image img-responsive" />
+              <div className="spash-page__preview-image-wrapper">
+                <img id="splash-cat" src="/previews/simon.png" className="splash-page__preview-image" />
+              </div>
+              <img id="iphone" src="/previews/phone.png" className="splash-page__preview-phone" />
             </a>
           </div>
           <div className="col m6 s12">
-              <img className="splash-page__logo" src="/logos/logo-reverse.png" />
+            <img className="splash-page__logo" src="/logos/logo-reverse.png" />
             <h1 className="splash-page__heading">{TAPi18n.__('app_name')}</h1>
             <h2 className="splash-page__description">{TAPi18n.__('app_description')}</h2>
 
